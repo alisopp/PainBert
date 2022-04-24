55941a9d98edefe170e6514afde824b6d4b59e43
npmExpress <104219596+npmExpress@users.noreply.github.com>
2022-04-24 15:19:51 +0200

    game over

--- Assets/Scripts/Main/ToolController.cs
+++ Assets/Scripts/Main/ToolController.cs
@@ -8,7 +8,9 @@ public class ToolController : MonoBehaviour
 {
     private static int _count = 0;
     private static int _overallCount = 0;
-    
+
+    public int paintCount = 3;
+    public int paintOverallCount = 10;
     public float painMultiplier = 1;
     
     public void SetToolQuestion(Question question)
@@ -16,14 +18,14 @@ public class ToolController : MonoBehaviour
         _count += (int) Math.Round(painMultiplier * 1);
         _overallCount += (int) Math.Round(painMultiplier * 1);;
 
-        if (_overallCount >= 10)
+        if (_overallCount >= paintOverallCount)
         {
             _count = 0;
             _overallCount = 0;
             UIManager.Instance.SetMenu(UIManager.Menu.GAME_OVER);
         }
         
-        if (_count >= 5)
+        if (_count >= paintCount)
         {
             _count = 0;
             DialogController.Instance.SetToolButtonQuestion(question);
